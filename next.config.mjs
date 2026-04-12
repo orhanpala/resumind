712611034bbaab31897b8cb1d3aca91bde97107a
orhan pala <palaorhan30@gmail.com>
2026-04-12 07:14:09 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
+  serverExternalPackages: ['jspdf', 'html2canvas', 'fflate', 'pdf2json', 'mammoth'],
 }
 
 export default nextConfig
